52ce36be6d761e5c285f2171789a8bc62ef9032b
Vagini A <vaginipy97@gmail.com>
2025-04-16 17:23:26 +0530

    connect using railway for deployment

--- db.php
+++ db.php
@@ -1,6 +1,13 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "contact_app");
+$host = "tramway.proxy.rlwy.net";
+$port = 22020;
+$user = "root";
+$pass = "lANJaSVbCXLTrjYsnlINHBXJartYmagA";
+$db   = "railway";
 
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
